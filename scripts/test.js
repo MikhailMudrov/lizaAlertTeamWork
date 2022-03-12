e9be7ec5a51c98b6d8133c0fda5dd553d53eba3a
Alexey Kondratjev <alexeykondratjev@yandex.ru>
2022-03-12 13:28:08 +0300

    Стр. Тест. В 1 приближ добавлена логика проверки теста

--- scripts/test.js
+++ scripts/test.js
@@ -2,8 +2,49 @@ import { createSideMenuHandlers } from "./side-menu.js";
 
 createSideMenuHandlers();
 
+let numberOfAttemts = 0;
+
 const checkboxInputs = document.querySelectorAll('.test__answer-checkbox');
 const radioInputs = document.querySelectorAll('.test__answer-radio');
+
+const showResultButton = document.querySelector('#showResultBtn');
+const retakeButton = document.querySelector('#retakeBtn');
+const forwardButton = document.querySelector('#forwardBtn');
+
+const resultTestPositive = document.querySelector('.result-test_type_positive');
+const resultTestNegative = document.querySelector('.result-test_type_negative');
+
+const correctAnswers = {
+  1: [true, true, true],
+  2: [false, true, false]
+};
+
+function changeShowResultButtonState() {
+  //Если выбрано хотя бы по одному ответу каждом вопросе - кнопка становится активной.
+  const checkboxChosen = document.querySelectorAll('.test__option_checkbox_active');
+  const radioChosen = document.querySelectorAll('.test__option_radio_active');
+
+ if (checkboxChosen.length && radioChosen.length) {
+    showResultButton.classList.remove('button_state_disabled');
+    showResultButton.classList.add('button_state_active');
+  } else {
+    showResultButton.classList.add('button_state_disabled');
+    showResultButton.classList.remove('button_state_active');
+  }
+}
+
+//Функция сравнивает выбор пользователя с условиями успешной сдачи
+//и возвращает результат теста в формате "Успех"/"Провал" (True/False).
+//Результат отрицательный, когда в первом вопросе выбрано 2 или 3 варианта ответа,
+//а во втором вопросе выбран неправильный вариант (первый или третий).
+//Результат положительный, когда в первом вопросе выбрано 2 или 3 варианта ответа,
+//а во втором вопросе выбран второй вариант.
+function getTestResult() {
+  const firstQuestionResult = document.querySelectorAll('.test__option_checkbox_active').length > 1;
+  const secondQuestionResult = document.querySelectorAll('.test__radio').item(1).classList.contains('test__option_radio_active');
+
+  return firstQuestionResult && secondQuestionResult;
+}
 
 
 checkboxInputs.forEach(function (checkBox) {
@@ -16,6 +57,9 @@ checkboxInputs.forEach(function (checkBox) {
       this.parentNode.classList.add('test__option_checkbox_notactive');
       console.log(this);
     }
+
+    //Активация кнопки "Показать результат".
+    changeShowResultButtonState();
   });
 });
 
@@ -32,5 +76,47 @@ radioInputs.forEach(function (radio) {
         element.parentNode.classList.add('test__option_radio_notactive');
       }
     } );
+
+    //Активация кнопки "Показать результат".
+    changeShowResultButtonState();
 });
 });
+
+//Отображение результата теста.
+showResultButton.addEventListener('click', function () {
+  //Увеличиваем счетчик количества попыток сдачи.
+  numberOfAttemts++;
+  /* console.log(numberOfAttemts); */
+
+  //Получим результат теста.
+  const testResult = getTestResult();
+
+  showResultButton.classList.add('button_hidden');
+  retakeButton.classList.remove('button_hidden');
+
+  if (testResult) {
+    //Отображаем карточку с позитивным результатом теста.
+    resultTestPositive.classList.remove('result-test_hidden');
+
+    //Активизируем кнопку "Далее".
+    forwardButton.classList.remove('button_state_disabled');
+    forwardButton.classList.add('button_state_active');
+    forwardButton.querySelector('img').setAttribute('src', './images/forward-arrow-active.svg');
+  } else {
+    //Отображаем карточку с негативным результатом теста.
+    resultTestNegative.classList.remove('result-test_hidden');
+
+    //Активизируем кнопку "Пересдать".
+    retakeButton.classList.remove('button_state_inactive');
+    retakeButton.classList.add('button_state_active');
+    retakeButton.querySelector('img').setAttribute('src', './images/retake-active.svg');
+
+    //Если пользователь трижды провалил тест - активизируем кнопку "Далее".
+    if (numberOfAttemts >= 3) {
+      forwardButton.classList.remove('button_state_disabled');
+      forwardButton.classList.add('button_state_active');
+      forwardButton.querySelector('img').setAttribute('src', './images/forward-arrow-active.svg');
+    }
+  }
+});
+
