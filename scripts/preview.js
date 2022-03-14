58a5045d31a9942bcb196c1610ddfecac70d43ee
Catherine Kuznetcova <kartin_life@mail.ru>
2022-03-14 22:31:09 +0300

    поправить ошибки в валидаторе. кроме test

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,3 +1,16 @@
 import {createSideMenuHandlers} from "./side-menu.js";
 
 createSideMenuHandlers();
+
+const goToTestButton = document.querySelector('#goToTest');
+const goToVideoButton = document.querySelector('#goToVideo');
+
+function goToTestButtonHandler() {
+  document.location.href = './test.html';
+}
+goToTestButton.addEventListener('click', goToTestButtonHandler);
+
+function goToVideoButtonHandler() {
+  document.location.href = './video.html';
+}
+goToVideoButton.addEventListener('click', goToVideoButtonHandler);
